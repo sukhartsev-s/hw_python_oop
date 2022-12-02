126a9c4fb8bf1b2eed5d89671127058e8a50b05f
Sergey Sukhartsev <bloxazavr@mail.ru>
2022-12-02 14:52:23 +0300

    flake8, pytest

--- homework.py
+++ homework.py
@@ -88,10 +88,10 @@ class SportsWalking(Training):
 
     def get_spent_calories(self) -> float:
         return ((self.CALORIES_WEIGHT_MULTIPLIER * self.weight
-                + ((((self.get_mean_speed() * self.KMH_IN_MSEC) ** 2)
+                + ((self.get_mean_speed() * self.KMH_IN_MSEC) ** 2
                  / (self.height / self.CM_IN_M))
                  * self.CALORIES_SPEED_HEIGHT_MULTIPLIER
-                 * self.weight) * (self.duration * self.MIN_IN_H)))
+                 * self.weight) * (self.duration * self.MIN_IN_H))
 
 
 class Swimming(Training):
