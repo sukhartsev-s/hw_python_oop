ea9838d26576d735685fd252267a8e8eae38b688
Sergey Sukhartsev <bloxazavr@mail.ru>
2022-12-04 14:36:52 +0300

    List, Dict, ClassVar

--- homework.py
+++ homework.py
@@ -1,8 +1,10 @@
+from typing import List, Dict, ClassVar
+
+
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-    def __init__(self, training_type, duration,
-                 distance, speed, calories) -> None:
-
+    def __init__(self, training_type: dict, duration: float,
+                 distance: float, speed: float, calories: float) -> None:
         self.training_type = training_type
         self.duration = duration
         self.distance = distance
@@ -20,11 +22,11 @@ class InfoMessage:
 
 class Training:
     """Базовый класс тренировки."""
-    LEN_STEP = 0.65
-    M_IN_KM = 1000
-    MIN_IN_H = 60
-    CALORIES_MEAN_SPEED_MULTIPLIER = 18
-    CALORIES_MEAN_SPEED_SHIFT = 1.79
+    LEN_STEP: ClassVar[float] = 0.65
+    M_IN_KM: ClassVar[float] = 1000
+    MIN_IN_H: ClassVar[float] = 60
+    CALORIES_MEAN_SPEED_MULTIPLIER: ClassVar[float] = 18
+    CALORIES_MEAN_SPEED_SHIFT: ClassVar[float] = 1.79
 
     def __init__(self, action: int, duration: float, weight: float) -> None:
         self.action = action
@@ -41,27 +43,20 @@ class Training:
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        pass
+        raise NotImplementedError("Необходимо определить get_spent_calories()")
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        return InfoMessage(self.__class__.__name__, self.duration,
+        return InfoMessage(self.__class__.__name__,
+                           self.duration,
                            self.get_distance(), self.get_mean_speed(),
                            self.get_spent_calories())
 
 
 class Running(Training):
     """Тренировка: бег."""
-    CALORIES_MEAN_SPEED_MULTIPLIER = 18
-    CALORIES_MEAN_SPEED_SHIFT = 1.79
-
-    def __init__(self,
-                 action: int,
-                 duration: float,
-                 weight: float) -> None:
-        super().__init__(action,
-                         duration,
-                         weight)
+    CALORIES_MEAN_SPEED_MULTIPLIER: ClassVar[float] = 18
+    CALORIES_MEAN_SPEED_SHIFT: ClassVar[float] = 1.79
 
     def get_spent_calories(self) -> float:
         return ((self.CALORIES_MEAN_SPEED_MULTIPLIER * self.get_mean_speed()
@@ -72,10 +67,10 @@ class Running(Training):
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
-    CALORIES_SPEED_HEIGHT_MULTIPLIER = 0.029
-    CALORIES_WEIGHT_MULTIPLIER = 0.035
-    KMH_IN_MSEC = 0.278
-    CM_IN_M = 100
+    CALORIES_SPEED_HEIGHT_MULTIPLIER: ClassVar[float] = 0.029
+    CALORIES_WEIGHT_MULTIPLIER: ClassVar[float] = 0.035
+    KMH_IN_MSEC: ClassVar[float] = 0.278
+    CM_IN_M: ClassVar[float] = 100
 
     def __init__(self,
                  action: int,
@@ -96,9 +91,9 @@ class SportsWalking(Training):
 
 class Swimming(Training):
     """Тренировка: плавание."""
-    LEN_STEP = 1.38
-    CALORIES_ADDITIONAL_MEAN_SPEED = 1.1
-    CALORIES_WEIGHT_MULTIPLIER = 2
+    LEN_STEP: ClassVar[float] = 1.38
+    CALORIES_ADDITIONAL_MEAN_SPEED: ClassVar[float] = 1.1
+    CALORIES_WEIGHT_MULTIPLIER: ClassVar[float] = 2
 
     def __init__(self,
                  action: int,
@@ -122,9 +117,9 @@ class Swimming(Training):
                 / self.M_IN_KM / self.duration)
 
 
-def read_package(workout_type: str, data: int):
+def read_package(workout_type: str, data: List[int]) -> Training:
     """Прочитать данные полученные от датчиков."""
-    training_type: dict = {
+    training_type: Dict[str, Training] = {
         'SWM': Swimming,
         'RUN': Running,
         'WLK': SportsWalking
@@ -132,7 +127,7 @@ def read_package(workout_type: str, data: int):
     return training_type[workout_type](*data)
 
 
-def main(training: Training) -> InfoMessage:
+def main(training: Training) -> None:
     """Главная функция."""
     info: InfoMessage = training.show_training_info()
     print(info.get_message())
